74737c44cef8886be2bddef0960473caad07a26f
RealG00D <146886024+RealG00D@users.noreply.github.com>
2025-05-24 10:53:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Клас HelloWorld демонструє простий приклад програми з виводом.
+ * @author Oleh Derevetskyi
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Основний метод, який виводить повідомлення "Hello, World!".
+     * @param args Аргументи командного рядка
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
